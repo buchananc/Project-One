92e85ad6c8166e01689330830ab668c2a5d83bf6
314napple <caleb.sears22@gmail.com>
2018-04-30 15:15:52 -0400

    adding init.js file to be included in each html doc (after linking firebase)

---
+++ assets/javascript/init.js
@@ -0,0 +1,15 @@
+// Initializing Firebase
+var config = {
+    apiKey: "AIzaSyCeZJWPPfaCh9IqHcd0SLw6AdKkH46qZRQ",
+    authDomain: "cross-bite.firebaseapp.com",
+    databaseURL: "https://cross-bite.firebaseio.com",
+    projectId: "cross-bite",
+    storageBucket: "cross-bite.appspot.com",
+    messagingSenderId: "789203245949"
+  };
+  firebase.initializeApp(config);
+
+const database = firebase.database();
+const auth = firebase.auth();
+// Creating a reference to my users in Firebase
+const usersRef = database.ref().child('users');
