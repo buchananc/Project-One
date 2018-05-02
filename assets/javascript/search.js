74d876a8481f4cc4a8f10001ba619923b6a1eef4
buchananc <candacebuchanan7@gmail.com>
2018-05-01 20:33:15 -0400

    adding search.html and it works

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,23 +1,21 @@
 /////////////// Global Variables /////////////////
 let userData = '';
 
-//Data object constructor
+///////////////Data object constructor/////////////////////////////////////////////
 function dataObj() {
     this.uid = '',
         this.foodID = '',
-        this.fromElement = '',
-        this.toElement = '',
         this.name = '',
         this.url = '',
         this.rating = '',
-        this.link = ''
+        this.link = '';
 }
 
-// --Rating star function, build up a star for each rating increment, return the appropriate html
+///////////////////Rating star function, build up a star for each rating increment////////////////////////
 function genRating(rating) {
     let html = `Rating: <b id='rating' value=${rating} <span>`;
     for (var i = 1, j = rating; i <= j; i++) {
-        html += '<i class="fa fa-star yellow" aria-hidden="true"></i>';
+        html += '<i class="fa fa-star" aria-hidden="true"></i>';
     }
     html += '</span></b>';
     return html;
@@ -25,13 +23,22 @@ function genRating(rating) {
 
 ////////////get array of results from Yummly, loop through, grab data//////////////
 function showFood(result, index, array) {
+    console.log(result);
     let foodID = result.id;
-    let recipe_name = result.recipeName = result.recipeName ? result.recipeName : 'Name';
+    let recipe_name = result.recipeName ? result.recipeName : 'Name';
     let rating = result.rating;
-
-    let link = `https://www.yummly.com/recipes?q=`;
-    let encoded_name = encodeURIComponent(recipe_name);
-    link += encoded_name;
+    let ingredientList = result.ingredients;
+    let cookTime = result.totalTimeInSeconds;
+    let formattedCookTime = moment.utc(cookTime * 1000).format('HH:mm:ss');
+    console.log(formattedCookTime);
+    console.log(ingredientList);
+    let ingredientListArray = [];
+    ingredientList.forEach(function(element) {
+         ingredientListArray.push(element);
+     });
+    let formattedIngredients = ingredientListArray.join('\n');
+    let link = `https://www.yummly.com/#recipe/${foodID}`;
+    console.log(link);
     let img = result.imageUrlsBySize[90];
 
     /////////////push results to html//////////////////////////
@@ -39,11 +46,26 @@ function showFood(result, index, array) {
         `<div class='card' id=${foodID} data-value=${foodID}` +
         `><div><span><i class='fa fa-hand-o-right' aria-hidden='true'></i></span>${recipe_name}` +
         "</div><div>" +
-        `<img src=${img}` + "><p>" +
-        genRating(rating) +
-        `</p></div><div><a href="${link}" target='_blank' class="external_link"><i class="fa fa-external-link" aria-hidden="true"></i> View Recipe </a></div>`);
+        `<img src=${img}` + "><p>" + //adds image
+        genRating(rating) + //adds rating stars
+        `</p></div><div><button type='button' class='btn btn-info btn-lg trigger testButton' id='button1'><i class="fa fa-external-link" aria-hidden="true"></i>View More Information</button><div class='modal-title-info'>${recipe_name}</div><div class='modal-body-info'>`+
+            `<p class='modalRating'>${rating}</p>` +
+            `<p class="modalCookTime">${formattedCookTime}</p>` +
+            `<p class="modalIngredientList">${formattedIngredients}</p>` +
+            `<button class="recipeButtonLink">` +
+            `<a href="${link}">View Recipe!</a></div></div>`);
+        
+    //////////////////create modal///////////////////
+    $(".testButton").off("click");
+    $(".testButton").on('click', function () {
+        console.log('hello');
+       // $("#myModal .modal-title").text(recipe_name);
+        $("#myModal .modal-title").empty().append($(this).siblings(".modal-title-info").clone());
+        $('#myModal .modal-body').empty().append($(this).siblings(".modal-body-info").clone());
+        $("#myModal").modal("show");
+    });
 }
-
+        
 //////////use searchAPI to search the Yummly API, return the recipe/food data jsonp object
 function searchAPI(recipe_search, food_search) {
     $('p.search_error').empty();
@@ -66,21 +88,20 @@ function searchAPI(recipe_search, food_search) {
 
     $.ajax({
         type: 'GET',
+        dataType: 'json',
         url: queryURL,
-        //dataType: 'jsonp'
     }).then(function (result) {
         console.log(result);
         let results_length = result.matches.length; //saves results as a variable
-        
+
         $('div.column_results').append(`Search Results (${results_length})`);
         result.matches.forEach(showFood);
-     }); 
+    });
 }
 
-
-// --Document Section--
+/////////////////////Document Section////////////////////////////////////
 $(document).ready(function () {
-    // Recipe/food search, search API from form
+    // Recipe/food search, search API
     $('#foodsearch').submit(function (event) {
         event.preventDefault();
         var recipe_search = $('select#recipe_search').val();
@@ -94,4 +115,4 @@ $(document).ready(function () {
             $('p.search_error').text("Please choose from either recipes OR food type.");
         }
     });
-});
+})
